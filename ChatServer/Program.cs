99d7fea9dd09f8b27f41ae6d66d55a367c790fbf
Jared Teng <jaredteng2002@gmail.com>
2025-03-25 01:39:40 +0800

    test gcp authentication

--- ChatServer/Program.cs
+++ ChatServer/Program.cs
@@ -24,6 +24,6 @@ if (!app.Environment.IsDevelopment())
 app.UseCors("CorsPolicy");
 app.UseRouting();
 // Add authentication or authorization between routing & endpoint
-app.MapGet("/", () => "Change 1");
+app.MapGet("/", () => "Change 2");
 app.MapHub<ChatHub>("/chat");
 app.Run();
